18d07d64b6be83406edac5fa0a9e58a541aca17b
Jasen Aprian <46961710+jazen4p@users.noreply.github.com>
2022-06-17 15:56:43 +0700

    Add files via upload

---
+++ Answer3.php
@@ -0,0 +1,66 @@
+<?php 
+    function http_request($url){
+        // persiapkan curl
+        $ch = curl_init(); 
+
+        // set url 
+        curl_setopt($ch, CURLOPT_URL, $url);
+        
+        // set user agent    
+        curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+        // return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+        // $output contains the output string 
+        $output = curl_exec($ch); 
+
+        // tutup curl 
+        curl_close($ch);      
+
+        // mengembalikan hasil curl
+        return $output;
+    }
+
+    $url="https://jsonplaceholder.typicode.com/photos";
+
+    $prof = http_request($url);
+
+    //ubah string JSON menjadi array
+    $prof = json_decode($prof, TRUE);
+    // print_r($prof[0]['url']);
+?>
+
+<html>
+    <head>
+        <title>JSON Curl Data</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h1>JSON Curl Data - API jsonplaceholder</h1>
+            <div class="row">
+                <?php foreach($prof as $res){?>
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <img src="<?php echo $res['url']?>" style="width: 100%; height: 100%">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <span><?php echo $res['title']?></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php }?>
+            </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
